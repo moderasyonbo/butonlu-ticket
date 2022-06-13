7c334a439bcb9e0aab48b2eb11742715d01fdfbb
novabotlist <93944142+moderasyonbo@users.noreply.github.com>
2022-06-13 14:53:13 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,14 @@
+const moment = require('moment');
+const Discord = require('discord.js');
+const prefix = "!"
+
+module.exports = client => {
+
+ 
+    
+    
+
+
+
+    console.log(`Başarıyla giriş yapıldı!`);
+}
